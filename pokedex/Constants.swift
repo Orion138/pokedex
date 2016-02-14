047c433eaacf7e96fc47b7e7029f263cd752595d
Orion138 <nathan.boosinger@gmail.com>
2016-02-14 14:18:05 -0600

    Added code for parsing all necessary data from the pokeapi

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Nathan Boosinger on 2/14/16.
+//  Copyright © 2016 Nathan Boosinger. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
